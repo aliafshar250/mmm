e0ff9444a49d2923ce8ed44d8d03268bce80a877
aliafshar250 <mehdizare800@mailfa.com>
2016-04-15 02:01:45 +0430

    Update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,1 @@
-local function run(msg, matches ) 
-  if matches[1] == "یوزر" then
-    return "telegram.me/"..msg.from.username
-  end
-end
-
-return {
-  patterns ={
-    "^(یوزر)$" 
- }, 
-  run = run 
-}
+(msg.from.username )..'\n'
