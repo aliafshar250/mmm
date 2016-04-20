f92be01d2235c3f76ad774a35d0b2510803d169b
aliafshar250 <mehdizare800@mailfa.com>
2016-04-20 15:36:53 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -205,7 +205,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {145173448,137791771},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
@@ -457,7 +457,7 @@ sudo2:@mohammad20162015
 !info
 نمایش اطلاعات شما
 
-!chat kick
+!
 حذف کردن تمام پیام های گروه
 
 !admins
